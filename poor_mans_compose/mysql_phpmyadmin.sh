44d4b343004e6d78ed7959b301bb615389b17dee
TeleDark <46258401+TeleDark@users.noreply.github.com>
2024-04-07 16:42:14 +0330

    Added Docker network

--- poor_mans_compose/mysql_phpmyadmin.sh
+++ poor_mans_compose/mysql_phpmyadmin.sh
@@ -3,11 +3,13 @@
 clean () {
     docker stop phpmyadmin db1
     docker rm phpmyadmin db1
+    docker network rm app
 }
 
 run () {
-    docker run --name db1 -d -e MYSQL_ROOT_PASSWORD=root -v db1:/var/lib/mysql mysql:8.2 
-    docker run --name phpmyadmin -d --link db1 -p 8080:80 -e PMA_HOST=db1 phpmyadmin
+    docker network create --driver bridge --subnet 10.1.1.0/24 --gateway 10.1.1.1 app
+    docker run --name db1 --network=app -d -e MYSQL_ROOT_PASSWORD=root -v db1:/var/lib/mysql mysql:8.2 
+    docker run --name phpmyadmin --network=app -d -p 8080:80 -e PMA_HOST=db1 phpmyadmin
 }
 
 case $1 in
